32a88d483792d778c1a608ef8c704fc61ff386d6
bagnisty12 <bagnisty12@tlen.pl>
2024-07-09 19:42:50 +0200

    API + Forms
    Co-authored-by: KrystianC-88 <KrystianC-88@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+
+#cookies -> przez 3 miesiące trzymaj te informacje: 35insdksdfjksdfj 
+#session -> traktuj jako dict
+
 name = "Andrzej"
 age = 0
+chat = ["czesc z tej strony adam"]
+def add_message(new):
+    # z sesji wyciągnij imie użytkownika
+    name = session.get("name","Anon")
+
+    # text: janusz: jazda jazda
+    text = f"{name} - {new}"
+    chat.append(text)
 
 def change_name(new):
     global name
-    name = new
+    session['name'] = new
+
 def change_age(new):
     global age
     age = new
     
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'haslo'
 #dekorator -> funkcja, która wywołuje inną funkcję w środku, POŚREDNIK
 @app.route("/")
-def indexa():
-    
-    return render_template("index.html", zmienna=name, wiek=age)
+def index():
+    name = session.get("name", "Anon")
+
+    return render_template("index.html", zmienna=name, wiek=age,chat=chat)
 
 @app.route("/test", methods=["POST"])
 def test():
@@ -32,6 +47,17 @@ def test():
         
 
     return redirect("/")
+
+@app.route("/send", methods = ["POST"])
+def send():
+    form = request.form
+
+    for name, value in form.items():
+        if name =="wiadomosc":
+            add_message(value)
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
     # localhost -> 0.0.0.0 -> TWÓJ ADRES IP, TWÓJ KOMPUTER
